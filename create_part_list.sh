6bff60404eb74a0fad44cce84ea1633ed844c3c5
jordanfirthussf <jordan.firth@afacademy.af.edu>
2023-01-18 21:43:22 -0700

    edits parts lists

--- create_part_list.sh
+++ create_part_list.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # combine lab part lists into a consolidated part list
-cat lab\ 01\ electrical\ power/parts.csv > parts.csv
-tail -n +2 lab\ 02\ camera\ payload/parts.csv >> parts.csv
-tail -n +2 lab\ 03\ communication/parts.csv >> parts.csv
-tail -n +2 lab\ 04\ attitude/parts.csv >> parts.csv
+cat lab\ 00\ metrology/parts00.csv > parts.csv
+tail -n +2 lab\ 01\ electrical\ power/parts01.csv >> parts.csv
+tail -n +2 lab\ 02\ camera\ payload/parts02.csv >> parts.csv
+tail -n +2 lab\ 03\ communication/parts03.csv >> parts.csv
+tail -n +2 lab\ 04\ attitude/parts04.csv >> parts.csv
