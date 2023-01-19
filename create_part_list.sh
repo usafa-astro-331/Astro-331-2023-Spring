e6dd3413c8aac10ad9b9b5c45eaebb718fd0c383
Jordan Firth <jordan.firth@afacademy.af.edu>
2023-01-18 21:27:17 -0700

    removed extra header lines from consolidated part lists.

--- create_part_list.sh
+++ create_part_list.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # combine lab part lists into a consolidated part list
 cat lab\ 01\ electrical\ power/parts.csv > parts.csv
-cat lab\ 02\ camera\ payload/parts.csv >> parts.csv
-cat lab\ 03\ communication/parts.csv >> parts.csv
-cat lab\ 04\ attitude/parts.csv >> parts.csv
+tail -n +2 lab\ 02\ camera\ payload/parts.csv >> parts.csv
+tail -n +2 lab\ 03\ communication/parts.csv >> parts.csv
+tail -n +2 lab\ 04\ attitude/parts.csv >> parts.csv
