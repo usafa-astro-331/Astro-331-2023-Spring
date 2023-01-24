54ee3a20e1e7bdc862e8b3ca40462cd6001b1aec
jordanfirthussf <jordan.firth@afacademy.af.edu>
2023-01-24 10:05:15 -0700

    added a space

--- create_part_list.sh
+++ create_part_list.sh
@@ -4,4 +4,4 @@ cat lab\ 00\ metrology/parts00.csv > parts.csv
 tail -n +2 lab\ 01\ electrical\ power/parts01.csv >> parts.csv
 tail -n +2 lab\ 02\ camera\ payload/parts02.csv >> parts.csv
 tail -n +2 lab\ 03\ communication/parts03.csv >> parts.csv
-tail -n +2 lab\ 04\ attitude/parts04.csv >> parts.csv
+tail -n +2 lab\ 04\ attitude/parts04.csv >> parts.csv 
