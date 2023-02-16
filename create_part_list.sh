e76caa27809523027940567cb4b2da668971a65a
jordanfirthussf <jordan.firth@afacademy.af.edu>
2023-02-15 22:44:15 -0700

    typo

--- create_part_list.sh
+++ create_part_list.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # combine lab part lists into a consolidated part list
-cat lab\ equipment/lab_equpiment.csv > parts.csv
+cat lab\ equipment/lab_equipment.csv > parts.csv
 tail -n +2 lab\ 00\ metrology/parts00.csv >> parts.csv
 tail -n +2 lab\ 01\ electrical\ power/parts01.csv >> parts.csv
 tail -n +2 lab\ 02\ camera\ payload/parts02.csv >> parts.csv
