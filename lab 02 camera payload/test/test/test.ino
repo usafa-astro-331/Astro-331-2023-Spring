baa3e0169698eedfa3e238f6d91a126ae54e5836
jordanfirthussf <jordan.firth@afacademy.af.edu>
2023-02-23 00:02:49 -0700

    test.ino should take a picture when a button on pin 0 is pressed

--- lab 02 camera payload/test/test/test.ino
+++ lab 02 camera payload/test/test/test.ino
@@ -31,92 +31,21 @@ const int SPI_CS = 7;
   ArduCAM myCAM( OV5642, SPI_CS );
 #endif
 
-void myCAMSaveToSDFile(){
-char str[8];
-byte buf[256];
-static int i = 0;
-static int k = 0;
-uint8_t temp = 0,temp_last=0;
-uint32_t length = 0;
-bool is_header = false;
-File outFile;
-//Flush the FIFO
-myCAM.flush_fifo();
-//Clear the capture done flag
-myCAM.clear_fifo_flag();
-//Start capture
-myCAM.start_capture();
-Serial.println(F("start Capture"));
-while(!myCAM.get_bit(ARDUCHIP_TRIG , CAP_DONE_MASK));
-Serial.println(F("Capture Done."));  
-length = myCAM.read_fifo_length();
-Serial.print(F("The fifo length is :"));
-Serial.println(length, DEC);
-if (length >= MAX_FIFO_SIZE) //384K
-{
-  Serial.println(F("Over size."));
-  return ;
-}
-if (length == 0 ) //0 kb
-{
-  Serial.println(F("Size is 0."));
-  return ;
-}
-//Construct a file name
-k = k + 1;
-itoa(k, str, 10);
-strcat(str, ".jpg");
-//Open the new file
-outFile = SD.open(str, O_WRITE | O_CREAT | O_TRUNC);
-if(!outFile){
-  Serial.println(F("File open faild"));
-  return;
-}
-myCAM.CS_LOW();
-myCAM.set_fifo_burst();
-while ( length-- )
-{
-  temp_last = temp;
-  temp =  SPI.transfer(0x00);
-  //Read JPEG data from FIFO
-  if ( (temp == 0xD9) && (temp_last == 0xFF) ) //If find the end ,break while,
-  {
-    buf[i++] = temp;  //save the last  0XD9     
-    //Write the remain bytes in the buffer
-    myCAM.CS_HIGH();
-    outFile.write(buf, i);    
-    //Close the file
-    outFile.close();
-    Serial.println(F("Image save OK."));
-    is_header = false;
-    i = 0;
-  }  
-  if (is_header == true)
-  { 
-    //Write image data to buffer if not full
-    if (i < 256)
-    buf[i++] = temp;
-    else
-    {
-      //Write 256 bytes image data to file
-      myCAM.CS_HIGH();
-      outFile.write(buf, 256);
-      i = 0;
-      buf[i++] = temp;
-      myCAM.CS_LOW();
-      myCAM.set_fifo_burst();
-    }        
-  }
-  else if ((temp == 0xD8) & (temp_last == 0xFF))
-  {
-    is_header = true;
-    buf[i++] = temp_last;
-    buf[i++] = temp;   
-  } 
-} 
-}
 
-void setup(){
+
+int INT_PIN = 0; //define interrupt pin to 0
+// volatile int state = LOW; // To make sure variables shared between an ISR
+//the main program are updated correctly,declare them as volatile.
+
+void setup() {
+  // pinMode(INT_PIN, INPUT_PULLUP); // LS18 hardware debounce chip has a built-in pullup resistor
+   attachInterrupt(digitalPinToInterrupt(INT_PIN), myCAMSaveToSDFile, FALLING);
+   //interrupt at pin 2 blink ISR when pin to change the value
+// } 
+
+
+
+// void setup(){
 uint8_t vid, pid;
 uint8_t temp;
 Wire.begin();
@@ -204,7 +133,9 @@ myCAM.InitCAM();
   myCAM.OV5642_set_JPEG_size(OV5642_320x240);
 #endif
 delay(1000);
-}
+} // end setup()
+
+
 void loop(){
 myCAMSaveToSDFile();
 delay(5000);
@@ -212,23 +143,91 @@ delay(5000);
 
 
 
-int pin = 2; //define interrupt pin to 2
-volatile int state = LOW; // To make sure variables shared between an ISR
-//the main program are updated correctly,declare them as volatile.
 
-void setup() {
-   pinMode(13, OUTPUT); //set pin 13 as output
-   attachInterrupt(digitalPinToInterrupt(pin), blink, CHANGE);
-   //interrupt at pin 2 blink ISR when pin to change the value
-} 
-void loop() { 
-   digitalWrite(13, state); //pin 13 equal the state value
-} 
-
-void blink() { 
-   //ISR function
-   state = !state; //toggle the state when the interrupt occurs
+void myCAMSaveToSDFile(){
+char str[8];
+byte buf[256];
+static int i = 0;
+static int k = 0;
+uint8_t temp = 0,temp_last=0;
+uint32_t length = 0;
+bool is_header = false;
+File outFile;
+//Flush the FIFO
+myCAM.flush_fifo();
+//Clear the capture done flag
+myCAM.clear_fifo_flag();
+//Start capture
+myCAM.start_capture();
+Serial.println(F("start Capture"));
+while(!myCAM.get_bit(ARDUCHIP_TRIG , CAP_DONE_MASK));
+Serial.println(F("Capture Done."));  
+length = myCAM.read_fifo_length();
+Serial.print(F("The fifo length is :"));
+Serial.println(length, DEC);
+if (length >= MAX_FIFO_SIZE) //384K
+{
+  Serial.println(F("Over size."));
+  return ;
 }
+if (length == 0 ) //0 kb
+{
+  Serial.println(F("Size is 0."));
+  return ;
+}
+//Construct a file name
+k = k + 1;
+itoa(k, str, 10);
+strcat(str, ".jpg");
+//Open the new file
+outFile = SD.open(str, O_WRITE | O_CREAT | O_TRUNC);
+if(!outFile){
+  Serial.println(F("File open faild"));
+  return;
+}
+myCAM.CS_LOW();
+myCAM.set_fifo_burst();
+while ( length-- )
+{
+  temp_last = temp;
+  temp =  SPI.transfer(0x00);
+  //Read JPEG data from FIFO
+  if ( (temp == 0xD9) && (temp_last == 0xFF) ) //If find the end ,break while,
+  {
+    buf[i++] = temp;  //save the last  0XD9     
+    //Write the remain bytes in the buffer
+    myCAM.CS_HIGH();
+    outFile.write(buf, i);    
+    //Close the file
+    outFile.close();
+    Serial.println(F("Image save OK."));
+    is_header = false;
+    i = 0;
+  }  
+  if (is_header == true)
+  { 
+    //Write image data to buffer if not full
+    if (i < 256)
+    buf[i++] = temp;
+    else
+    {
+      //Write 256 bytes image data to file
+      myCAM.CS_HIGH();
+      outFile.write(buf, 256);
+      i = 0;
+      buf[i++] = temp;
+      myCAM.CS_LOW();
+      myCAM.set_fifo_burst();
+    }        
+  }
+  else if ((temp == 0xD8) & (temp_last == 0xFF))
+  {
+    is_header = true;
+    buf[i++] = temp_last;
+    buf[i++] = temp;   
+  } 
+} 
+} // end mycam.savetosdfile
 
 
 
